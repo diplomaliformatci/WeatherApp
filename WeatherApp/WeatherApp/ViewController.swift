96076ed202e5d8c6ce4fc741b82bddf710165e2f
Can Kincal <can.kincal@efectura.com>
2019-11-21 18:30:01 +0300

    [Feature] - Project created

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Can Kincal on 21.11.2019.
+//  Copyright © 2019 Can Kincal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
